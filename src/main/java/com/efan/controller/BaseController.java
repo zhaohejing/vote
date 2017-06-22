f5a9f18db2e00089a055c6aebb59ad86bff6942f
454258314 <454258314@qq.com>
2017-06-22 16:08:24 +0800

    init project

--- src/main/java/com/efan/controller/BaseController.java
+++ src/main/java/com/efan/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.efan.controller;
 
+import com.efan.core.page.ActionResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by 45425 on 2017/6/22.
  */
 @RestController
-public class BaseController {
+public class BaseController  {
     public Logger logger = LogManager.getLogger(getClass());
+  /*  public BaseController() throws TokenException{
+        String token="";
+        if (token==null){
+            throw  new TokenException();
+
+        }
+    }
+    public  class TokenException extends Exception{
+
+    }*/
 }
